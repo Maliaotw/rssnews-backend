980d9b0e52238102f3d313005b31b84bc2a496fd
Maliao Guo <MaliaoTW@gmail.com>
2022-03-27 19:17:46 +0800

    優化快取緩存功能

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,6 +6,34 @@ import google.auth
 from google.cloud import secretmanager
 from google.oauth2 import service_account
 
+# https://docs.djangoproject.com/en/dev/ref/settings/#debug
+DEBUG = env.bool("DEBUG", False)
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
+SECRET_KEY = env("DJANGO_SECRET_KEY")
+
+# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+
+
+
+# DATABASES
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("MYSQL_DATABASE"),
+        'HOST': os.environ.get("MYSQL_HOST"),
+        'PORT': os.environ.get("MYSQL_PORT"),
+        'USER': os.environ.get("MYSQL_USER"),
+        'PASSWORD': os.environ.get("MYSQL_ROOT_PASSWORD"),
+        'OPTIONS': {'charset': 'utf8mb4'},
+    }
+}
+
+
+
 GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
     BASE_DIR / 'credentials.json'
 )
@@ -19,3 +47,4 @@ MEDIA_URL = f'https://storage.googleapis.com/{GS_MEDIA_BUCKET_NAME}/'
 # GS_DEFAULT_ACL = 'publicRead'
 # GS_QUERYSTRING_AUTH = False
 # GS_DEFAULT_ACL = None
+
